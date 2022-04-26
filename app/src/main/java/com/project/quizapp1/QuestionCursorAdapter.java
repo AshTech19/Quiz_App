8ad2f8287018801bce8636786b271762aeb4e700
AYESHA <ayesha19ab@gmail.com>
2022-04-25 20:18:43 -0400

    Refactor: Answer Check Function to display the result

--- app/src/main/java/com/project/quizapp1/QuestionCursorAdapter.java
+++ app/src/main/java/com/project/quizapp1/QuestionCursorAdapter.java
@@ -2,11 +2,15 @@ package com.project.quizapp1;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.cursoradapter.widget.CursorAdapter;
 
@@ -20,6 +24,7 @@ public class QuestionCursorAdapter extends CursorAdapter {
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
         return LayoutInflater.from(context).inflate(R.layout.question_item,parent,false);
+
     }
 
     @Override
@@ -28,21 +33,47 @@ public class QuestionCursorAdapter extends CursorAdapter {
         RadioButton btn1 = view.findViewById(R.id.Option1);
         RadioButton btn2 = view.findViewById(R.id.Option2);
         RadioButton btn3 = view.findViewById(R.id.Option3);
+        RadioGroup rbGroup = view.findViewById(R.id.groupBtn);
+        Button submitBtn = view.findViewById(R.id.btnSubmit);
+        TextView result = view.findViewById(R.id.score);
+
+
 
         int questionColumnIndex = cursor.getColumnIndex(QuestionEntry.COLUMN_QUESTION);
         int option1ColumnIndex = cursor.getColumnIndex(QuestionEntry.COLUMN_OPTION1);
         int option2ColumnIndex = cursor.getColumnIndex(QuestionEntry.COLUMN_OPTION2);
         int option3ColumnIndex = cursor.getColumnIndex(QuestionEntry.COLUMN_OPTION3);
+        int answerColumnIndex = cursor.getColumnIndex(QuestionEntry.COLUMN_ANSWER);
 
         String question = cursor.getString(questionColumnIndex);
         String option1 = cursor.getString(option1ColumnIndex);
         String option2 = cursor.getString(option2ColumnIndex);
         String option3 = cursor.getString(option3ColumnIndex);
+        int answer = cursor.getInt(answerColumnIndex);
 
         viewQuestion.setText(question);
         btn1.setText(option1);
         btn2.setText(option2);
         btn3.setText(option3);
 
+        submitBtn.setOnClickListener(v-> {
+            if(btn1.isChecked() || btn2.isChecked() || btn3.isChecked()){
+                RadioButton rbSelected = view.findViewById(rbGroup.getCheckedRadioButtonId());
+                int answerNo = rbGroup.indexOfChild(rbSelected) + 1;
+                if(answerNo == answer){
+                    result.setText(R.string.correct_answer);
+                    result.setTextColor(Color.GREEN);
+                } else {
+                    result.setText(R.string.wrong_answer);
+                    result.setTextColor(Color.RED);
+                }
+            } else {
+                Toast.makeText(context, "Please select an answer", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
     }
+
 }
